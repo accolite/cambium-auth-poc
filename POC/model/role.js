484074036a3f9a0cde4b06abe018a0b7b3e46c1d
Krishna <krishna.parachikapu@accolite.com>
2016-10-03 23:28:48 +0530

    <Initial version of oauth2 poc>

--- POC/model/role.js
+++
@@ -1,9 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var RoleSchema   = new Schema({
-    roleName : String,
-    permissionId : Schema.Types.ObjectId
-});
-
-module.exports = mongoose.model('Role', RoleSchema);
--- POC/routes/blogs.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/read', function(req, res, next) {
-    res.json({info : "api version 0.0.1", url :  req.url});
-});
-
-module.exports = router;
